953993bd185be50e3a04c216504b61a4c5131110
Delphinegln <delphinegallon52@gmail.com>
2025-12-08 16:16:17 +0100

    Update code streamlit.py

--- code streamlit.py
+++ code streamlit.py
@@ -2162,12 +2162,6 @@ if st.session_state["show_bowser_page"]:
         with tab5:
             st.subheader("📊 Tableau Complet")
             
-            # Options d'affichage
-            col1, col2 = st.columns(2)
-            with col1:
-                show_greeks = st.checkbox("Afficher les Greeks", value=True)
-            with col2:
-                decimals = st.slider("Décimales", 2, 6, 2)
             
             # Préparation du tableau
             df_display = df_results.copy()
